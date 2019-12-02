c0c39fee7fea2efb6ce0a316639b7450f5bb43ac
nouvelle <nouvellenuts@gmail.com>
2019-12-03 00:17:25 +0900

    created: Create Project Form(until #9)

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -19,15 +19,15 @@ class SignIn extends Component {
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
           <h5 className="grey-text text-darken-3">Sign In</h5>
-          <div className="input-fild">
+          <div className="input-field">
             <label htmlFor="email">Email</label>
             <input type="email" id="email" onChange={this.handleChange} />
           </div>
-          <div className="input-fild">
+          <div className="input-field">
             <label htmlFor="password">Password</label>
             <input type="password" id="password" onChange={this.handleChange} />
           </div>
-          <div className="input-fild">
+          <div className="input-field">
             <button className="btn pink lighten-1 x-depth-0">Login</button>
           </div>
         </form>
