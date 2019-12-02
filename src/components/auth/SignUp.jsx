4851e5f25a2be93453b938a56e29838f848c3722
nouvelle <nouvellenuts@gmail.com>
2019-12-03 00:05:22 +0900

    created: Login & Signup Components(until #8)

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 
-class SignIn extends Component {
+class SignUp extends Component {
   state = {
     email: "",
-    password: ""
+    password: "",
+    firstName: "",
+    lastName: ""
   };
   handleChange = e => {
     this.setState({
@@ -28,7 +30,15 @@ class SignIn extends Component {
             <input type="password" id="password" onChange={this.handleChange} />
           </div>
           <div className="input-fild">
-            <button className="btn pink lighten-1 x-depth-0">Login</button>
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" onChange={this.handleChange} />
+          </div>
+          <div className="input-fild">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" onChange={this.handleChange} />
+          </div>
+          <div className="input-fild">
+            <button className="btn pink lighten-1 x-depth-0">Sign up</button>
           </div>
         </form>
       </div>
@@ -36,4 +46,4 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+export default SignUp;
