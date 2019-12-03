2ef3143ba68d56785de54e46d8fb6b1923aaee69
nouvelle <nouvellenuts@gmail.com>
2019-12-03 21:16:07 +0900

    created: Tracking Auth Status(until #25)

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -5,25 +5,23 @@ import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
 const Navbar = props => {
-  // const { auth } = props;
-  // console.log(auth);
+  const { auth } = props;
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           MarioPlan
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = state => {
-  // console.log(state);
   return {
-    // auth: state.firebase.auth
+    auth: state.firebase.auth
   };
 };
 
