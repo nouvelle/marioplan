eba2440303b86571673ee593e1db9b49b8dc815b
nouvelle <nouvellenuts@gmail.com>
2019-12-03 20:48:33 +0900

    created:  Logging Users In(until #23)

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn } from "../../store/actions/authActions";
 
 class SignIn extends Component {
   state = {
@@ -12,9 +14,11 @@ class SignIn extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
+    this.props.signIn(this.state);
     console.log(this.state);
   };
   render() {
+    const { authError } = this.props;
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -29,6 +33,9 @@ class SignIn extends Component {
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 x-depth-0">Login</button>
+            <div className="red-text center">
+              {authError ? <p>{authError}</p> : null}
+            </div>
           </div>
         </form>
       </div>
@@ -36,4 +43,16 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+const mapStateToProps = state => {
+  return {
+    authError: state.auth.authError
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signIn: creds => dispatch(signIn(creds))
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -5,12 +5,12 @@ import "firebase/auth";
 // Replace this with your own config details
 const config = {
   apiKey: "AIzaSyCNxBGxrPEydgV1KXYyiJSjHuLzxVtCfdk",
-  authDomain: "marioplan-f0734.firebaseapp.com",
-  databaseURL: "https://marioplan-f0734.firebaseio.com",
-  projectId: "marioplan-f0734",
-  storageBucket: "marioplan-f0734.appspot.com",
-  messagingSenderId: "93088772232",
-  appId: "1:93088772232:web:042b5f66e52ff76e0c6f3d"
+  // authDomain: "marioplan-f0734.firebaseapp.com"
+  // databaseURL: "https://marioplan-f0734.firebaseio.com",
+  projectId: "marioplan-f0734"
+  // storageBucket: "marioplan-f0734.appspot.com",
+  // messagingSenderId: "93088772232",
+  // appId: "1:93088772232:web:042b5f66e52ff76e0c6f3d"
 };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
