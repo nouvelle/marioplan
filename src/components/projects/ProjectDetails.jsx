d8a44d1e5efd4004c176042b6087e6a603a3853c
nouvelle <nouvellenuts@gmail.com>
2019-12-03 14:12:12 +0900

    created: Project Details Data(until #20)

--- src/components/projects/ProjectDetails.jsx
+++ src/components/projects/ProjectDetails.jsx
@@ -1,25 +1,47 @@
 import React from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 const ProjectDetails = props => {
-  const id = props.match.params.id;
-  return (
-    <div className="container section project-details">
-      <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-title">Project Title - {id} </span>
-          <p>
-            Hey gang, in this React, Redux & Firebase tutorial we'll create the
-            template for our project details component. We'll also set up a
-            route to load the component in.
-          </p>
-          <div className="card-action gret lighten-4 grey-text">
-            <div>Posted by The Net Ninja</div>
-            <div>2nd September, 2am</div>
+  const { project } = props;
+  if (project) {
+    return (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
+            <div className="card-action gret lighten-4 grey-text">
+              <div>
+                Posted by {project.authorFirstName} {project.authorLastName}
+              </div>
+              <div>2nd September, 2am</div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container center">
+        <p>Loading project...</p>
+      </div>
+    );
+  }
 };
 
-export default ProjectDetails;
+const mapStateToProps = (state, ownProps) => {
+  // console.log(state);
+  const id = ownProps.match.params.id;
+  const projects = state.firestore.data.projects;
+  const project = projects ? projects[id] : null;
+  return {
+    project: project
+  };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "projects" }])
+)(ProjectDetails);
