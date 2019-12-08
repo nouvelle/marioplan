ae772705112deedf760b314c7047255f0d1949da
nouvelle <nouvellenuts@gmail.com>
2019-12-08 20:15:38 +0900

    created: Adding Projects(until #30)

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -2,7 +2,7 @@ const initState = {};
 
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
-    case "CREATE_PROJECT_SUCCESS":
+    case "CREATE_PROJECT":
       console.log("create project success");
       return state;
     case "CREATE_PROJECT_ERROR":
